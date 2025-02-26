03bd40734ce5f0204bd8a82a126cfb93cf5c9f85
Madhwansh08 <Madhwansh.Srivastava@merai.co>
2025-02-26 15:28:36 +0530

    Basic server code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log("Server here")
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 6000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/uploadimages', (req, res) => {
+  const { leftImage, rightImage } = req.body;
+  // Mock response for image upload
+  res.json({ message: 'Images successfully uploaded!', leftImage, rightImage });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
